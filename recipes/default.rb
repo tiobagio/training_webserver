7f33704974a535151e2bd7d3477f23cbde730aae
Tio Bagio <tio.bagio@gmail.com>
2019-04-07 13:39:06 +0800

    initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook:: webserver
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'httpd' do
+  action :install
+end
+
+template '/var/www/html/index.html' do
+    source 'index.html.erb'
+end
+
+template '/etc/httpd/conf/httpd.conf' do
+    source 'httpd.conf.erb'
+    notifies :restart, 'service[httpd]'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
